4abb85ea27514a94df0fd8be040826c96d51d309
Brian Winkers <bwinkers@gmail.com>
2017-10-21 14:31:56 -0700

    Initial class to kick things off

---
+++ src/NuggetClass.php
@@ -0,0 +1,24 @@
+<?php
+
+class NuggetClass
+{
+
+    /**
+     * Create a new Skeleton Instance
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Test function
+     *
+     * @param string $phrase Phrase to return
+     *
+     * @return string Returns the phrase passed in
+     */
+    public function echoPhrase($phrase)
+    {
+        return $phrase;
+    }
+}
