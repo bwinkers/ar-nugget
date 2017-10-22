b5e6863fe820f6a07fd6f2482879d3f8fcec4b31
Brian Winkers <bwinkers@gmail.com>
2017-10-21 18:31:21 -0700

    Initial test stub

---
+++ tests/Activerules/Nugget/NuggetTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Activerules\Nugget;
+
+use PHPUnit\Framework\TestCase;
+
+class NuggetTest extends TestCase {
+
+    public function testHello() {
+        $this->assertEquals('Hello', 'Hell' . 'o');
+    }
+
+}
