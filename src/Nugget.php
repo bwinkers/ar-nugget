e702ac535fe663917857f07229ad421dee55a704
Brian Winkers <bwinkers@gmail.com>
2018-01-14 10:16:42 -0800

    Added core property types

--- src/Nugget.php
+++ src/Nugget.php
@@ -9,12 +9,85 @@ use Activerules\Nugget\Exceptions\NuggetException;
  */
 class Nugget
 {
+  
+    protected $types = [
+        'integer' => [
+          'type' => 'integer',
+          'format' => 'int32',
+          'comment' => 'signed 32 bits'  
+        ],
+        'long' => [
+          'type' => 'integer',
+          'format' => 'int64',
+          'comment' => 'signed 64 bits'  
+        ],
+        'float' => [
+            'type' => 'number',
+            'format' => 'float'
+        ],
+        'double' => [
+            'type' => 'number',
+            'format' => 'double'
+        ],
+        'string' =>[
+            'type' => 'string'
+        ],
+        'byte' =>[
+            'type' => 'string',
+            'format' => 'byte',
+            'comment' => 'base64 encoded characters'
+        ],
+        'binary' =>[
+            'type' => 'string',
+            'format' => 'binary',
+            'comment' => 'any sequence of octets'
+        ],
+        'boolean' => [
+            'type' => ' boolean'
+        ],
+        'date' =>[
+            'type' => 'string',
+            'format' => 'date',
+            'comment' => 'As defined by full-date - RFC3339'
+        ],
+        'datetime' =>[
+            'type' => 'string',
+            'format' => 'date-ime',
+            'comment' => 'As defined by date-time - RFC3339'
+        ],
+        'password' =>[
+            'type' => 'string',
+            'comment' => 'A hint to UIs to obscure input.'
+        ],
+        'url' => [
+            'type' => 'string'
+        ],
+        'text' => [
+            'type' => 'string'
+        ]
+    ];
+    
+    
 
     /**
      * Create a new Skeleton Instance
      */
     public function __construct()
     {
+    }
+    
+    /**
+     * 
+     * @param type $type
+     * @return type
+     */
+    public function jsonType($type) {
+      $type = strtolower($type);
+      if(isset($this->types[$type])) {
+        return $this->types[$type]['type'];
+      }
+      
+      return false;
     }
     
     /**
@@ -57,5 +130,7 @@ class Nugget
         
         return $jsonOut ? json_encode($cleanObject) : $cleanObject;
     }
+    
+    
 
 }
