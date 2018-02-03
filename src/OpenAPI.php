7acf9aecefd7ae93dea1c0711d12750c0e5c4b82
Brian Winkers <bwinkers@gmail.com>
2018-02-03 11:13:14 -0800

    Load JSON types as needed

--- src/OpenAPI.php
+++ src/OpenAPI.php
@@ -12,23 +12,20 @@ class OpenAPI
     protected $types;
 
     /**
-     * Constructor
-     */
-    public function __construct()
-    {
-        include realpath(__DIR__.'/lookup/nuggetTypes.php');
-
-        $this->types = $types;
-    }
-
-    /**
      * Get the JSON type for a higher-level Nugget type
      * @param string $type
      * @return mixed string of mapped JSON type or boolean FALSE
      */
     public function jsonType($type) {
+      
         if (!is_string($type)) {
-            throw new \Activerules\Nugget\Exceptions\NuggetException('Invalid Type');
+            throw new \Activerules\Nugget\Exceptions\NuggetException('Invalid type, it must be a string.');
+        }
+        
+        if(!is_array($this->types)) {
+          include realpath(__DIR__.'/lookup/nuggetTypes.php');
+
+          $this->types = $types;
         }
 
         $type = strtolower($type);
