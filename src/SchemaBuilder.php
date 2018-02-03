03b102ab69f2282fb343b5540650c706cea7fd6a
Brian Winkers <bwinkers@gmail.com>
2018-02-03 09:31:12 -0800

    removed jsonType, in OpenAPI now

--- src/SchemaBuilder.php
+++ src/SchemaBuilder.php
@@ -63,24 +63,6 @@ class SchemaBuilder
 
         return str_replace($this->filesys->cleanPath($targetPath), $this->filesys->cleanPath($replacementPath), $JSON);
     }
-
-    /**
-     * Get the JSON type for a higher-level Nugget type
-     * @param string $type
-     * @return mixed string of mapped JSON type or boolean FALSE
-     */
-    public function jsonType($type) {
-        if (!is_string($type)) {
-            throw new \Activerules\Nugget\Exceptions\NuggetException('Invalid Type');
-        }
-
-        $type = strtolower($type);
-        if (isset($this->types[$type])) {
-            return $this->types[$type]['type'];
-        }
-
-        return false;
-    }
     
     /**
      *
