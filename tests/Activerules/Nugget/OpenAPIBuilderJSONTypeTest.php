ad54dba47d51bc1ae2ef944c75e722f025d5b064
Brian Winkers <bwinkers@gmail.com>
2018-02-03 09:28:18 -0800

    Move Schemadoc JSON type conversion to OpenAPI

---
+++ tests/Activerules/Nugget/OpenAPIBuilderJSONTypeTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Activerules\Nugget;
+
+use PHPUnit\Framework\TestCase;
+
+
+// Need more test
+
+
+class OpenAPIJSONTypeTest extends TestCase {
+  
+    /**
+     * This gets called before all test functions
+     */
+    public function setUp() 
+    {
+        // Use the Activerules dereferencer
+        $dereferencer  = \Activerules\JsonReference\Dereferencer::draft4();
+        
+        $this->openAPI = new \Activerules\Nugget\OpenAPI();
+            
+    }
+    
+    /**
+     * An object missing required fields should fail.
+     */
+    public function testInvalidTypeFails() 
+    {
+        $result = $this->openAPI->jsonType('fooglesnaps');
+
+        $this->assertEquals(false, $result);
+    }
+    
+    /**
+     * An object with valid core data should pass nugget validation
+     */
+    public function testValidTypePasses() 
+    {
+        $result = $this->openAPI->jsonType('Text');
+
+        $this->assertEquals('string', $result);
+    }
+}
