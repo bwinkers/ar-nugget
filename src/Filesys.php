d69ddc861af68d6217e602156bf480b56f6b24ac
Brian Winkers <bwinkers@gmail.com>
2018-02-02 22:15:14 -0800

    Moved file functions to own file

---
+++ src/Filesys.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Activerules\Nugget;
+
+use Activerules\Nugget\Exceptions\NuggetException;
+
+/**
+ * The Nugget 
+ */
+class Filesys
+{
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Write a file out
+     * 
+     * @param string $data
+     * @param string $path
+     */
+    public function writeFile($data, $path){
+        $filePath = fopen($path, 'w');
+
+        // Write the spec to the file pointer
+        fwrite($filePath, $data);
+
+        // Close the file pointer
+        fclose($filePath);
+    }
+      
+    /**
+     * Making sure the trailing slash in path is consistent.
+     * 
+     * @param string $path
+     */
+    public function cleanPath($path) {
+      return rtrim($path, '/').'/';
+    }
+    
+    /**
+     * Filter out dot files and directories
+     * 
+     * @param string $fileInfo
+     * @return boolean
+     */
+    public function realDirFile($fileInfo)
+    {
+        $name = $fileInfo->getFilename();
+
+        $isValid = false;
+
+        if (!$fileInfo->isDot() && !$fileInfo->isDir() && substr($name, 0, 1) != '.') {
+            $isValid = true;
+        }
+
+        return $isValid;
+    }
+}
