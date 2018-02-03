acc650fa5d8399a03e6075f0f28d976577ac0f83
Brian Winkers <bwinkers@gmail.com>
2018-02-03 12:46:34 -0800

    Fixed to work with formatting

--- tests/Activerules/Nugget/SchemaBuilderConvertSchemaFileRefsTest.php
+++ tests/Activerules/Nugget/SchemaBuilderConvertSchemaFileRefsTest.php
@@ -4,37 +4,36 @@ namespace Activerules\Nugget;
 
 use PHPUnit\Framework\TestCase;
 
-
 // Need more test
 
 
-class SchemaBuilderConvertSchemaFileRefsTest extends TestCase {
-  
+class SchemaBuilderConvertSchemaFileRefsTest extends TestCase
+{
     /**
      * This gets called before all test functions
      */
-    public function setUp() 
+    public function setUp()
     {
         $this->builder = new \Activerules\Nugget\SchemaBuilder();
     }
-    
+
     /**
      * A trailing slash path has a single trailing slash at the end
      */
-    public function testConvertingRefsWorks() 
+    public function testConvertingRefsWorks()
     {
-      // Define input and outpur schema directories
-      $schemaDir = realpath('./tests/Activerules/Nugget/schema/');
-      $schemaOut = realpath('./tests/Activerules/Nugget/schemaOut/');
-      
-      // Run the nugget to copnvert the schema $refs
-      $this->builder->convertSchemaFileRefs($schemaDir, $schemaOut, 'https://schema.izzup.com/', '#/components/schema/');
-      
-      // Read the created file contents
-      $file = realpath('./tests/Activerules/Nugget/schemaOut/Test.json');
-      $JSON = file_get_contents($file);
-      
-       $this->assertEquals('{
+        // Define input and outpur schema directories
+        $schemaDir = realpath('./tests/Activerules/Nugget/schema/');
+        $schemaOut = realpath('./tests/Activerules/Nugget/schemaOut/');
+
+        // Run the nugget to copnvert the schema $refs
+        $this->builder->convertSchemaFileRefs($schemaDir, $schemaOut, 'https://schema.izzup.com/', '#/components/schema/');
+
+        // Read the created file contents
+        $file = realpath('./tests/Activerules/Nugget/schemaOut/Test.json');
+        $JSON = file_get_contents($file);
+
+        $this->assertEquals(preg_replace('/\v(?:[\v\h]+)/', '', '{
     "title": "Test",
     "description": "For testing only",
     "properties": {
@@ -44,9 +43,7 @@ class SchemaBuilderConvertSchemaFileRefsTest extends TestCase {
         }
     },
     "type": "object"
-}', $JSON);
-      
-    
+}'), preg_replace('/\v(?:[\v\h]+)/', '', $JSON));
     }
 
 }
