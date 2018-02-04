9851bef5f737c5a945c4f3cf91e58d5ce366a228
Brian Winkers <bwinkers@gmail.com>
2018-02-04 12:27:39 -0800

    Added GSheetReader

---
+++ src/GSheetReader.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Activerules\Nugget;
+
+use Activerules\Nugget\Exceptions\NuggetException;
+
+
+/**
+ * The Nugget anonymous Google Spreadsheet reader.
+ * This reads data from a public spreadsheet.
+ */
+class GSheetReader
+{
+    public $gSheet;
+
+    /**
+     *
+     * @param array $credentials
+     * @param array $scopes
+     */
+    public function __construct($url)
+    {
+        $gSheet = $this->loadGsheetCSV($url);
+        $this->gSheet = $gSheet;
+    }
+    
+    /**
+     * Fetch CSV data from public Google Spreadsheet
+     * @param type $url
+     * @return object
+     */
+    public function loadGsheetCSV($url)
+    {
+       $file= file_get_contents($url);
+       return $file;
+    }
+
+    /**
+     *
+     * @return object
+     */
+    public function gSheet()
+    {
+        return $this->gSheet;
+    }
+}
