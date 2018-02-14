f47e1dfaffd74333fde859c382922150f6d3938a
Brian Winkers <bwinkers@gmail.com>
2018-02-13 23:35:01 -0800

    Added new base exception

---
+++ src/Exception/NuggetInvalidArgumentException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Activerules\Nugget\Exception;
+
+/**
+ * Class NuggetInvalidArgumentException
+ * Exception thrown if an argument is not of the expected type.
+ * 
+ * @package Activerules\Nugget\Exception
+ */
+class NuggetInvalidArgumentException extends \InvalidArgumentException
+{
+    
+}
