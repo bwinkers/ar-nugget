1ca5e5395059bda4c79c06ec4c6f56e300876178
Brian Winkers <bwinkers@gmail.com>
2018-02-13 23:34:44 -0800

    Added new base exception

---
+++ src/Exception/NuggetBadMethodCallException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Activerules\Nugget\Exception;
+
+/**
+ * Class BadMethodCallException
+ * Exception thrown if a callback refers to an undefined method or if some arguments are missing.
+ * 
+ * @package Activerules\Nugget\Exception
+ */
+class NuggetBadMethodCallException extends \BadMethodCallException
+{
+    
+}
