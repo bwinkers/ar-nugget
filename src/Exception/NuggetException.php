87c0531bc9d808dbc83c63562aa3425bca54798a
Brian Winkers <bwinkers@gmail.com>
2018-02-13 22:45:11 -0800

    Change exception name space

---
+++ src/Exception/NuggetException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Activerules\Nugget\Exception;
+
+/**
+ * Class NuggetException
+ * @package Activerules\Nugget\Exception
+ */
+class NuggetException extends \Exception
+{
+    
+}
