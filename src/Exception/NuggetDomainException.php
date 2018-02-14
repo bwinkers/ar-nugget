e8271ab948fa53cddaa4365c4d93413b6bbcb6b7
Brian Winkers <bwinkers@gmail.com>
2018-02-13 23:34:53 -0800

    Added new base exception

---
+++ src/Exception/NuggetDomainException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Activerules\Nugget\Exception;
+
+/**
+ * Class NuggetDomainException
+ * Exception thrown if a value does not adhere to a defined valid data domain.
+ * 
+ * @package Activerules\Nugget\Exception
+ */
+class NuggetDomainException extends \DomainException
+{
+    
+}
